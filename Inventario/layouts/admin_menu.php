0d04ff785056a3e9cfeeddc8b9adc0d091230546
DanielMG1 <danielmelangiraldo@gmail.com>
2020-05-27 23:03:33 -0500

    Sistema de Inventario Alturas

---
+++ Inventario/layouts/admin_menu.php
@@ -0,0 +1,61 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Panel de control</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>Accesos</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Administrar grupos</a> </li>
+      <li><a href="users.php">Administrar usuarios</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categorías</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Elementos</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Administrar Elementos</a> </li>
+       <li><a href="add_product.php">Agregar Elementos</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Media</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Prestamos</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Administrar prestamos</a> </li>
+         <li><a href="add_sale.php">Realizar prestamos</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Reporte de prestamos</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Prestamos por fecha </a></li>
+        <li><a href="monthly_sales.php">Prestamos mensuales</a></li>
+        <li><a href="daily_sales.php">Prestamos diarios</a> </li>
+      </ul>
+  </li>
+</ul>

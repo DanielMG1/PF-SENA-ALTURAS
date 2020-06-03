8a3e725f4de3fd08cb9916cb6d722153e7d0975a
sebasco58 <jscastano56@misena.edu.co>
2020-06-03 10:16:29 -0500

    actualizacion 3 de junio header home

--- Inventario_alturas/views/home.php
+++ Inventario_alturas/views/home.php
@@ -1,3 +1,115 @@
-<?php
-    /* Home de la página. Depende del inicio de sesión */
-?>
+<?php include_once('../layouts/header.php'); ?>
+    
+<section id="Card-area">
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="Card-single">
+                        <div class="Card-single-item text-center">
+
+                            <div class="icon">
+                                <i class="icon-gear"></i>
+                            </div>
+                            <h3>Ultimos Registros</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto quibusdam ducimus? Ad illo, ab voluptatum numquam ea quidem earum qui ipsum asperiores.alert-dismissible   
+                            </p>
+                        </div>
+
+                        <img class="hover-shape-1 hover-shape" src="../src/icon/shape-one.svg" alt="Shape One">
+                        <img class="hover-shape-2 hover-shape" src="../src/icon/shape-two.svg" alt="Shape Two">
+                        <img class="hover-shape-3 hover-shape" src="../src/icon/shape-three.svg" alt="Shape Three">
+                        <img class="hover-shape-4 hover-shape" src="../src/icon/shape-four.svg" alt="Shape Four">
+                        <img class="hover-shape-5 hover-shape" src="../src/icon/shape-five.svg" alt="Shape Five">
+
+
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="Card-single">
+                        <div class="Card-single-item two text-center">
+
+                            <div class="icon">
+                                <i class="icon-web-design"></i>
+                            </div>
+                            <h3>Herramientas Activas</h3>
+                            <p> 
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor nisi cum aut, exercitationem error, cupiditate accusamus ex beatae.
+                            </p>
+                        </div>
+
+                        <img class="hover-shape-1 hover-shape" src="../src/icon/shape-one.svg" alt="Shape One">
+                        <img class="hover-shape-2 hover-shape" src="../src/icon/shape-two.svg" alt="Shape Two">
+                        <img class="hover-shape-3 hover-shape" src="../src/icon/shape-three.svg" alt="Shape Three">
+                        <img class="hover-shape-4 hover-shape" src="../src/icon/shape-four.svg" alt="Shape Four">
+                        <img class="hover-shape-5 hover-shape" src="../src/icon/shape-five.svg" alt="Shape Five">
+
+
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="Card-single">
+                        <div class="Card-single-item three text-center">
+
+                            <div class="icon">
+                                <i class="icon-report"></i>
+                            </div>
+                            <h3>Herremientas para Mantenimiento</h3>
+                            <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem accusamus voluptas animi corrupti exercitationem molestiae obcaecati ducimus maiores quaerat perspiciatis! 
+                            </p>
+                        </div>
+
+                        <img class="hover-shape-1 hover-shape" src="../src/icon/shape-one.svg" alt="Shape One">
+                        <img class="hover-shape-2 hover-shape" src="../src/icon/shape-two.svg" alt="Shape Two">
+                        <img class="hover-shape-3 hover-shape" src="../src/icon/shape-three.svg" alt="Shape Three">
+                        <img class="hover-shape-4 hover-shape" src="../src/icon/shape-four.svg" alt="Shape Four">
+                        <img class="hover-shape-5 hover-shape" src="../src/icon/shape-five.svg" alt="Shape Five">
+
+
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="Card-single">
+                        <div class="Card-single-item four text-center">
+
+                            <div class="icon">
+                                <i class="icon-list"></i>
+                            </div>
+                            <h3>Usuarios Agregados</h3>
+                            <p> 
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque atque ullam sed unde, ipsa repudiandae totam placeat nostrum aspernatur doloremque, magnam quisquam.      
+                            </p>
+                        </div>
+
+                        <img class="hover-shape-1 hover-shape" src="../src/icon/shape-one.svg" alt="Shape One">
+                        <img class="hover-shape-2 hover-shape" src="../src/icon/shape-two.svg" alt="Shape Two">
+                        <img class="hover-shape-3 hover-shape" src="../src/icon/shape-three.svg" alt="Shape Three">
+                        <img class="hover-shape-4 hover-shape" src="../src/icon/shape-four.svg" alt="Shape Four">
+                        <img class="hover-shape-5 hover-shape" src="../src/icon/shape-five.svg" alt="Shape Five">
+
+
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="load-more-btn text-center">
+                        <a href="">Mas...</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>

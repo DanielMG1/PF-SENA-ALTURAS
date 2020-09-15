73e8c5b5a2dbe06a3011cdd27d7f3ea89f373d5d
sebasco58 <jscastano56@misena.edu.co>
2020-09-15 11:22:51 -0500

    Actualizacion base de datos alturas

--- ALTURAS-BETA/altura.sql
+++ ALTURAS-BETA/altura.sql
@@ -2,10 +2,10 @@
 -- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 25-08-2020 a las 19:19:49
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-09-2020 a las 18:15:10
 -- Versión del servidor: 10.4.8-MariaDB
--- Versión de PHP: 7.3.10
+-- Versión de PHP: 7.2.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -40,15 +40,10 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `descripcion`, `foto`) VALUES
-(1, 'Cascos', 'Tipo de laso', ''),
-(2, 'Correas', 'Riatas y correas', ''),
-(3, 'Ganchos', 'Los ganchos', NULL),
-(4, 'Otros ganchos', 'los ganchos', NULL),
-(5, 'Otros ganchos', 'los ganchos', NULL),
-(6, 'Correas', 'las correas', NULL),
-(7, 'arnes', 'arnes', NULL),
-(8, 'Rescate', 'Equipos de rescate', NULL),
-(9, 'Riatas', 'las riatas', NULL);
+(10, 'Arnés', 'Utilizado en el área de alturas', NULL),
+(11, 'Poleas', 'Equipo Utilizado en el area de alturas', NULL),
+(13, 'Lasos', 'Equipo utilizado en el área de alturas \r\n', NULL),
+(14, 'Guantes', 'Equipo de alturas Requerido', NULL);
 
 -- --------------------------------------------------------
 
@@ -76,8 +71,13 @@ CREATE TABLE `herramientas` (
 --
 
 INSERT INTO `herramientas` (`id`, `nombre`, `marca`, `longitud`, `serie`, `descripcion`, `cantidad`, `foto`, `entidad_cert`, `fecha_fbc`, `norma_cert`, `vencimiento`) VALUES
-(1, 'Pastel', 'textil', 12, 24243243, 'lowrnfasklf', 4, NULL, 'alturas', NULL, '123213', '4556'),
-(2, 'gthkh', 'textil', 12, 24243243, 'utjgxcjuyhc.kh', 12, NULL, 'alturas', NULL, '123213', '4556');
+(1, 'Arnes', 'textil', 12, 24243243, 'lowrnfasklf', 12, NULL, 'alturas', '', '123213', '2020-08-14'),
+(6, 'Cuerda Vida', 'textil', 12, 24243243, 'obg{log{o', 3, NULL, 'alturas', '2020-08-30', '123213', '2020-07-27'),
+(7, 'Cascos', 'Mors', 23, 123424, 'lorems sinpu', 23, NULL, 'ausgkjbasd', '2020-08-14', 'saf', '2020-08-07'),
+(8, 'Polea', 'Alturas', 12, 0, 'adstfdashg', 2, NULL, 'caesrsyd', '2020-08-12', 'cc45', '2020-08-27'),
+(9, 'Lasos de', 'alturas', 12, 24243243, 'Equipo utilizado en alturas', 3, NULL, 'alturas', '2020-09-10', '123213', '2020-09-03'),
+(10, 'Ganchos', 'alturas', 12, 8743, 'Equipo utilizado en el área de alturas', 4, NULL, 'Sena', '2020-09-04', 'jhdfj647', '2020-09-01'),
+(12, 'Herramienta altura', 'alturas ', 653, 67534, 'Equipo de alturas', 5, NULL, '45trr6', '2020-09-04', 'hgds677', '2020-09-11');
 
 -- --------------------------------------------------------
 
@@ -99,8 +99,10 @@ CREATE TABLE `prestamos` (
 --
 
 INSERT INTO `prestamos` (`id`, `fecha`, `herramienta_id`, `cantidad`, `usuario_id`, `devuelto`) VALUES
-(1, '2020-08-25 00:00:00', 1, 5, 6, 0),
-(2, '2020-08-25 00:00:00', 2, 4, 1, 1);
+(9, '2020-09-03 00:00:00', 8, 1, 2, 0),
+(12, '2020-09-17 00:00:00', 9, 20, 11, 1),
+(16, '2020-09-05 00:00:00', 1, 5, 6, 0),
+(17, '2020-09-05 00:00:00', 6, 4, 15, 0);
 
 -- --------------------------------------------------------
 
@@ -141,10 +143,16 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `email`, `rol_id`, `password`) VALUES
 (1, 'Daniel', 'daniel@mail.com', 1, '$2y$10$TNkKcN3aryXBOzE6vbjiA.8Uc109rT0HR1N8ij1hbhOaZaoRo6rpW'),
-(2, 'Leonardo', 'leo@mail.com', 2, '$2y$10$TNkKcN3aryXBOzE6vbjiA.8Uc109rT0HR1N8ij1hbhOaZaoRo6rpW'),
-(3, 'Diana', 'diana@mail.com', 2, '$2y$10$TNkKcN3aryXBOzE6vbjiA.8Uc109rT0HR1N8ij1hbhOaZaoRo6rpW'),
-(5, 'Luis Miguel', 'migue@mail.com', 2, '$2y$12$0VzHALCz0vNsVitq1xr77O/RKup1zeqd9hJu/hovcQButN21tkVim'),
-(6, 'Andrea', 'andre@mail.com', 2, '$2y$12$DEVdJMXgdchW0FCOO9u8O.jUmY8xA8MBamI56nH/5nfPeI5exx0N.');
+(2, 'Sebastian', 'sebasco@gmail.co', 1, '$2y$12$y82MsouFa0sULKaml3GEROmFX.dIph91D8yxdWTpapVV/EbmwsBLa'),
+(6, 'Prueba3', 'prueba3@mail.com', 2, '$2y$12$Yj7xcYkVTHGFhBWjrOMmCe3BUB7SGDE0aOf5dPjzlEvUSNtNlViBm'),
+(10, 'Prueba6', 'prueba6@mail.com', 2, '$2y$12$AnLXV/cVcqfh0hmKZQwqyeK6dd/FZ3dlHfNPp9h2B4/ldoO9dvO3O'),
+(11, 'Prueba7', 'prueba7@mail.com', 2, '$2y$12$YTH7XOBz/o0D2ix6eXKL..1ZNOtwdVScF9dFYmGbxY3pFYzthmbtK'),
+(12, 'prueba8', 'prueba8@mail.com', 2, '$2y$12$V9/XysPsm5FngGWRbgoi8usvkvUOPyaXzjdJu6PXuV/wU1ivyoxjW'),
+(14, 'Pruba8', 'prueba9@mail.co', 1, '$2y$12$b9/RUyJqzOw9OH.HKBjtnOxS.QL4yZTYFtaYYBrpKpZ4.qLrcXV9a'),
+(15, 'Prueba10', 'prueba10@mail.com', 2, '$2y$12$/VtsfdkNq1Ksb0LehBR5K.BsMID8qlDUP5lWprjx1D9f77B3dhKYS'),
+(16, 'Prueba11', 'prueba11@mail.com', 2, '$2y$12$NwHs.KWFZK4G.przgl3/Cu/xbITszbf.O4wfL1J.rRehzeyU1XDya'),
+(17, 'Prueba12', 'prueba12@mail.co', 2, '$2y$12$No.HZmI33vySzDUBSea6e.NEroZicticXZJ/YUEVvjY9kCb2kM6Xe'),
+(18, 'Prueba13', 'prueba13@mail.co', 2, '$2y$12$SgJB/Fmyl82pW0lCuVn3GOQg5ZFjkfXlW0cbQOA1CFdxoqkjvTI2m');
 
 --
 -- Índices para tablas volcadas
@@ -192,13 +200,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `herramientas`
+--
+ALTER TABLE `herramientas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `prestamos`
 --
 ALTER TABLE `prestamos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -210,7 +224,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Restricciones para tablas volcadas
@@ -220,8 +234,8 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `prestamos`
 --
 ALTER TABLE `prestamos`
-  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`herramienta_id`) REFERENCES `herramientas` (`id`),
-  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`herramienta_id`) REFERENCES `herramientas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `usuarios`
